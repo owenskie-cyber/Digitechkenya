9d929f3e444311c92f083bf8dccefa8b63d73d05
owenskie-cyber <owendantez355@gmail.com>
2025-06-14 18:40:45 +0300

    Add files via upload
    Signed-off-by: owenskie-cyber <owendantez355@gmail.com>

--- sw.js
+++ sw.js
@@ -1,1 +1,1 @@
-// Empty Service Worker
+self.addEventListener('install', (e) => {e.waitUntil(caches.open('spice-connect-cache').then((cache) => {return cache.addAll(['/', '/index.html', '/manifest.json', '/sw.js']);}));});self.addEventListener('fetch', (e) => {e.respondWith(caches.match(e.request).then((response) => {return response || fetch(e.request);}));});
